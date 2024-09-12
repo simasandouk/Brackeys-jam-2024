4965ece877908e9d0c1effec94a3eb198b08a444
Lime <nabeltabaa2007@gmail.com>
2024-09-12 21:30:23 +0300

    Movement

--- Assets/Sima/Scripts/PlayerScript.cs
+++ Assets/Sima/Scripts/PlayerScript.cs
@@ -5,23 +5,51 @@ using UnityEngine.Animations;
 
 public class PlayerScript : MonoBehaviour
 {
-    private Vector3 direction; // normalized movement vector
-    public float movementSpeed;
-    // Update is called once per frame
+    private Vector2 direction;
+    public float Movement_speed;
+    public Rigidbody2D rb;
+    [SerializeField] float Dash_speed = 10;
+    [SerializeField] float Dash_duration = 1f;
+    [SerializeField] float Dash_cooldown = 1f;
+    bool Is_dashing;
+    bool Can_dash;
+
+    void Start()
+    {
+        Can_dash = true;
+    }
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Is_dashing) return;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-            direction.x = -1;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            direction.x = 1;
+            direction.x = Input.GetKey(KeyCode.A) ? -1 : 1;
         }
         else
         {
-            direction.x = direction.y = direction.z = 0;
+            direction.x = 0;
         }
-        transform.position += movementSpeed * Time.deltaTime * direction;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Can_dash)
+        {
+            StartCoroutine(Dash());
+        }
+        else
+        {
+            rb.velocity = new Vector2(Movement_speed * Time.deltaTime * direction.x * 10, rb.velocity.y);
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        Can_dash = false;
+        Is_dashing = true;
+        rb.velocity = new Vector2(Dash_speed * Time.deltaTime * direction.x * 10, rb.velocity.y);
+        yield return new WaitForSeconds(Dash_duration);
+        Is_dashing = false;
+        yield return new WaitForSeconds(Dash_cooldown);
+        Can_dash = true;
     }
 }
