fed293a39cee1c1d9ab7a445d801d4340f0069f4
Sandouk <111926705+simasandouk@users.noreply.github.com>
2024-09-14 14:14:26 +0300

    Functional doors babe

--- Assets/Sima/Scripts/DoorScript.cs
+++ Assets/Sima/Scripts/DoorScript.cs
@@ -6,36 +6,30 @@ public class DoorScript : MonoBehaviour
 {
     public Sprite opened;
     public Sprite closed;
-    public SpriteRenderer spriteRenderer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
 
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Key collided with door");
-        if (collision.gameObject.name == "Key")
+        if (collision.gameObject.layer == 6)
         {
-            // Collision detected with the specific object
-
+            // Collision detected with the opener object
+            Debug.Log(collision.gameObject.name + " opened the door");
+            OpenDoor();
+            Destroy(collision.gameObject);
         }
     }
 
     void OpenDoor()
     {
-        spriteRenderer.sprite = opened;
+        gameObject.GetComponent<SpriteRenderer>().sprite = opened;
+        gameObject.GetComponent<Rigidbody2D>().simulated = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
     }
 
-    void CloseDoor()
+    /*void CloseDoor()
     {
-        spriteRenderer.sprite = closed;
-    }
+        gameObject.GetComponent<SpriteRenderer>().sprite = closed;
+        gameObject.GetComponent<Rigidbody2D>().simulated = true;
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }*/
 }
